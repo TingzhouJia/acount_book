d71109d8ab5acddbc162c7025331009f4d2f0f20
TingzhouJia <joe.tzjia@gmail.com>
2020-05-11 23:57:17 -0400

    accounts ui

--- src/components/AccountBar/accountBar.tsx
+++ src/components/AccountBar/accountBar.tsx
@@ -1,26 +1,17 @@
-import React, { useEffect } from 'react'
-import { Tabs } from 'antd';
+import React, { useEffect, useState } from 'react'
+import { Tabs, Tag } from 'antd';
 import './accountBar.css'
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'redux/store';
 import { fetchAccountList } from 'redux/reducer/plaidReducer';
 import Loading from 'components/loader/loading';
-const renderTabs = (title: string) => {
-    return (
-        <div className="render_account_card">
-            <span style={{ fontWeight: 700 }}>{title}</span>
-            <span style={{ fontWeight: 300, fontSize: "0.4rem" }}>
-                CURRENT RECORD:{" "}
-            </span>
-            <span style={{ textAlign: "center", fontWeight: 700, fontSize: "1rem" }}>
-                24
-        </span>
+import { AccountEach } from 'redux/model/account';
+const ColorList = ['#de6262, #ffb88c', '#bdc3c7,#2c3e50', '#ffafbd,#ffc3a0', '#cc2b5e, #753a88', '#dd5e89,#f7bb97', '#eecda3,#ef629f', '#000428 ,#004e92',
+    '#ddd6f3 ,#faaca8', '#7b4397,#dc2430', '#4568dc ,#b06ab3'
+]
 
-        </div>
-    );
-};
 const AccountBar: React.FC = () => {
-
+    const [activeTab, setactiveTab] = useState<string>("0")
     const dispatch = useDispatch()
     const { plaidLoading, accountList } = useSelector((state: RootState) => state.plaid)
     useEffect(() => {
@@ -29,30 +20,46 @@ const AccountBar: React.FC = () => {
         }
 
     }, [dispatch, accountList])
+    const renderTabs = (item: AccountEach, index: number) => {
+        return (
+            <div onClick={()=>setactiveTab(`${index}`)} className="render_account_card" style={{ background: `linear-gradient(to right,${ColorList[index % ColorList.length]})` }}>
+                <span style={{ fontWeight: 700,display:'flex',justifyContent:"flex-start" }}><Tag style={{borderRadius:"5px"}} color='error'>{item?.balances.iso_currency_code}</Tag> {item.balances.available ? item.balances.available : item.balances.current}</span>
+                <span style={{display:'flex',justifyContent:"flex-start"}}>{item.type}</span>
+                <div className="render_card_bottom">
+                    <span>.... {item?.mask}</span>
+                    <span>{item?.name}</span>
+                </div>
+    
+            </div>
+        );
+    };
+
     return (
         <div className="account_page_body" style={{ padding: "1vw" }}>
-            {plaidLoading ? <Loading /> : <>
+            {plaidLoading ? <Loading /> :
                 <Tabs
-                    defaultActiveKey="1"
+
+                    activeKey={activeTab}
                     tabPosition="left"
-                    type="card"
+                    size="large"
+                    style={{ height: "85vh" }}
                 >
-                    <Tabs.TabPane tab={renderTabs("SUBSCRPTION")} key="1">
-                        <div className="account_tab_wrap">
+                    {
 
-                        </div>
-                    </Tabs.TabPane>
-                    <Tabs.TabPane tab={renderTabs("SUBSCRPTION")} key="2">
-                        <div className="account_tab_wrap">
+                        accountList?.accounts?.map((item: AccountEach, index: number) => (
+                            <Tabs.TabPane tab={renderTabs(item, index)} key={index.toString()} >
+                                <div className="account_tab_wrap">
+                        <span>{item.official_name}</span>
+                                </div>
+                            </Tabs.TabPane>
 
-                        </div>
-                    </Tabs.TabPane>
-                    <Tabs.TabPane tab={renderTabs("SUBSCRPTION")} key="3">
-                        <div className="account_tab_wrap">
+                        ))
 
-                        </div>
-                    </Tabs.TabPane>
-                </Tabs></>}
+
+                    }
+
+
+                </Tabs>}
         </div>
     )
 }
