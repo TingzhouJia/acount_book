373f6bf9176af7ea68a3588006add673f52f5680
TingzhouJia <joe.tzjia@gmail.com>
2020-05-15 23:52:26 -0400

    new record account form

--- src/components/inputForm/Information.tsx
+++ src/components/inputForm/Information.tsx
@@ -1,40 +1,24 @@
 import React, { useState, useReducer, useCallback, useEffect } from 'react'
-import { Form, Input, Button, DatePicker, message } from 'antd'
+import { Form, Input, Button, DatePicker, message, Select } from 'antd'
 import { outgoingList, incomeList } from '../Utils/utils'
 import { Icons } from '../../iconRes/icons';
-import { useDispatch } from 'react-redux'
-import { useHistory } from 'react-router-dom'
+
 import './information.css'
-import ChoiceBar from './choiceBar';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 
-
-// const init:ReducerState={Description:'',price:0,date:'',tags:[''],icon:'',type:''}
-// type ReducerState={
-//   Description:string,
-//   price:number,
-//   date:String,
-//   tags:[String],
-//   icon:String,
-//   type:String
-
-// }
-// type recuerAction=|{type:'discription',data:string}|{type:'price',data:number}|{type:'date',data:String}|{type:'type',data:String}|
-// {type:'icon',data:String}|{type:'reload',data:null}
-// function reducer(state:ReducerState,action:recuerAction):ReducerState{
-//   switch(action.type){
-//     case 'discription':return {...state,Description:action.data};
-//     case 'price':return {...state,price:action.data};
-//     case 'date':return {...state,date:action.data};
-//     case 'type':return {...state,type:action.data};
-//     case 'icon':return {...state,icon:action.data};
-//     case 'reload':return {...init};
-//   }
-// }
+const {  RangePicker } = DatePicker;
 interface Choice {
   choice: number
 }
-
-
+interface PriceValue {
+  num?: number,
+  duration?: 'monthly' | 'weekly' | 'seasonly'
+}
+interface PriceValueProps {
+  value: PriceValue,
+  onChange: (value: PriceValue) => void
+}
+const { Option } = Select
 const Information = (props: Choice) => {
 
 
@@ -47,28 +31,125 @@ const Information = (props: Choice) => {
   }
 
   const UtilityForm = () => {
-    return (
-      <Form form={form} className="utilities_form">
-        <div>
-        <span>Utilities Information</span>
-        <Form.Item label="Utilities Official Name" name="offical_name" rules={[
-          {
-            required: true,
-            message: 'Please input the offical name!',
-          }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item name="remark_name" label="Utilities Remark Name">
-          <Input />
-        </Form.Item>
-        </div>
-       <div>
-       <span>User Information</span>
-        <Form.Item name="user_name" label="Utilities Account Username">
-          <Input />
-        </Form.Item>
+    const [number, setNumber] = useState(0);
+    const [duration, setDuration] = useState('monthly');
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 40 },
+        sm: { span: 40 },
+      },
+      wrapperCol: {
+        xs: { span: 40 },
+        sm: { span: 40 },
+      },
+    };
+    const formItemLayoutWithOutLabel = {
+      wrapperCol: {
+        xs: { span: 40, offset: 0 },
+        sm: { span: 40, offset: 0 },
+      },
+    };
 
-       </div>
+    return (
+      <Form form={form} style={{padding:"1vw"}} onFinish={(values)=>console.log(values)} >
+        <div className="utilities_form">
+        <div style={{paddingRight:"2vw"}}>
+          <span>Utilities Information</span>
+          <Form.Item label="Utilities Official Name" name="offical_name" rules={[
+            {
+              required: true,
+              message: 'Please input the offical name!',
+            }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item name="remark_name" label="Utilities Remark Name">
+            <Input />
+          </Form.Item>
+          <Form.Item name="payment" label="Utilities Payment " rules={[{ required: true, message: 'Please provide statement value' }]}>
+            <span>
+              <Input
+                type="number"
+                placeholder="payment"
+
+                style={{ width: 100 }}
+              />
+              <Select
+                placeholder="select duration"
+                style={{ width: 150, margin: '0 8px' }}
+
+              >
+                <Option value="monthly">Monthly</Option>
+                <Option value="seasonly">Seasonly</Option>
+                <Option value="weekly">Weekly</Option>
+              </Select>
+            </span>
+          </Form.Item>
+          <Form.Item name="range-picker" label="RangePicker" rules={[{ type: 'array', required: true, message: 'Please select time!' }]}>
+            <RangePicker />
+          </Form.Item>
+        </div>
+        <div>
+          <span>User Information</span>
+          <Form.List name="names">
+        {(fields, { add, remove }) => {
+          return (
+            <div>
+              {fields.map((field, index) => (
+                <Form.Item
+                  {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+                  label={index === 0 ? 'Primary Contact' : 'Candidate Contact'}
+                  required={false}
+                  key={field.key}
+                >
+                  <Form.Item
+                    {...field}
+                    validateTrigger={['onChange', 'onBlur']}
+                    rules={[
+                      {
+                        required: true,
+                        whitespace: true,
+                        message: "Please input contact's name or delete this field.",
+                      },
+                    ]}
+                    noStyle
+                  >
+                    <Input placeholder="contact name" style={{ width: '80%' }} />
+                  </Form.Item>
+                 
+                  {fields.length > 1 ? (
+                    <MinusCircleOutlined
+                      className="dynamic-delete-button"
+                      style={{ margin: '0 8px' }}
+                      onClick={() => {
+                        remove(field.name);
+                      }}
+                    />
+                  ) : null}
+                </Form.Item>
+              ))}
+              <Form.Item>
+                <Button
+                  type="dashed"
+                  onClick={() => {
+                    add();
+                  }}
+                  style={{ width: '100%' }}
+                >
+                  <PlusOutlined /> Add field
+                </Button>
+              </Form.Item>
+            </div>
+          );
+        }}
+      </Form.List>
+
+        </div>
+        </div>
+        <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
       </Form>
     )
   }
@@ -111,7 +192,7 @@ const Information = (props: Choice) => {
 
   return (
     <div className="normal_info">
-      <UtilityForm/>
+      <UtilityForm />
     </div>)
 
 }
